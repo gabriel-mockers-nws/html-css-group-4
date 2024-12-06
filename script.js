21574b6b6432e51a5196e32adf4305b2769f505d
Crista-Desuraune <cdesuraunecocu@normandiewebschool.fr>
2024-12-06 15:29:25 +0100

    modif admin

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function toggleDarkMode() {
   document.body.classList.toggle("dark-mode");
 }
+
+function supprimerPost(postId) {
+  var postElement = document.getElementById(postId);
+  if (postElement) {
+    postElement.remove(); // Supprime l'élément du DOM
+  }
+}
